a6de3963bb20c1110d6b76acf991868cc0380973
Hosung Song <hoson@microsoft.com>
2018-08-23 14:39:15 -0700

    Fully automate Payara admin server, slave setup

--- payara-ubuntu-cluster/scripts/setup_payara.sh
+++ payara-ubuntu-cluster/scripts/setup_payara.sh
@@ -18,13 +18,7 @@ function download_payara
     cd $HOME_DIR
     rm -rf payara5
     wget http://central.maven.org/maven2/fish/payara/distributions/payara/5.182/payara-5.182.zip
-    unzip payara-5.182.zip
-}
-
-function start_payara_admin_server
-{
-    cd $HOME_DIR/payara5
-    sudo -u $PAYARA_ADMIN ./asadmin start-domain production
+    sudo -u $PAYARA_ADMIN unzip payara-5.182.zip
 }
 
 function install_ssh_private_key
@@ -36,15 +30,77 @@ function install_ssh_private_key
     chmod 400 $PAYARA_ADMIN_ID_RSA_PATH
 }
 
+function setup_payara_admin_server
+{
+    local admin_password=${1}
+
+    cd $HOME_DIR/payara5/bin
+    sudo -u $PAYARA_ADMIN ./asadmin start-domain production
+    cat <<EOF > .passwordfile
+AS_ADMIN_PASSWORD=
+AS_ADMIN_NEWPASSWORD=$admin_password
+EOF
+    sudo -u $PAYARA_ADMIN ./asadmin --user admin --passwordfile .passwordfile change-admin-password
+
+    echo "AS_ADMIN_PASSWORD=$admin_password" > .passwordfile
+
+    sudo -u $PAYARA_ADMIN ./asadmin --user admin --passwordfile .passwordfile enable-secure-admin
+    sudo -u $PAYARA_ADMIN ./asadmin stop-domain production
+    sudo -u $PAYARA_ADMIN ./asadmin start-domain production
+
+    rm -f .passwordfile
+}
+
+function setup_payara_servers
+{
+    local admin_password=${1}
+    local node_name_prefix=${2}
+    local node_count=${3}
+
+    cd $HOME_DIR/payara5/bin
+
+    echo "AS_ADMIN_PASSWORD=$admin_password" > .passwordfile
+
+    for (( i = 0; i < $node_count; i++ )); do
+        local node_name="${node_name_prefix}${i}"
+        local server_name="PayaraServer${i}"
+
+        sudo -u $PAYARA_ADMIN ./asadmin --user admin --passwordfile .passwordfile create-node-ssh --nodehost $node_name --installdir $HOME_DIR/payara5 --nodedir $HOME_DIR/nodes --sshuser $PAYARA_ADMIN --sshkeyfile $PAYARA_ADMIN_ID_RSA_PATH --install true $server_name
+    done
+
+    sudo -u $PAYARA_ADMIN ./asadmin --user admin --passwordfile .passwordfile copy-config default-config cluster-config
+
+    for (( i = 0; i < $node_count; i++ )); do
+        local server_name="PayaraServer${i}"
+        local instance_name="PS${i}-I1"
+
+        sudo -u $PAYARA_ADMIN ./asadmin --user admin --passwordfile .passwordfile create-instance --node $server_name --config cluster-config $instance_name
+    done
+
+    sudo -u $PAYARA_ADMIN ./asadmin --user admin --passwordfile .passwordfile create-deployment-group payara-dg
+
+    for (( i = 0; i < $node_count; i++ )); do
+        local instance_name="PS${i}-I1"
+
+        sudo -u $PAYARA_ADMIN ./asadmin --user admin --passwordfile .passwordfile add-instance-to-deployment-group --instance $instance_name --deploymentgroup payara-dg
+    done
+
+    sudo -u $PAYARA_ADMIN ./asadmin --user admin --passwordfile .passwordfile start-deployment-group payara-dg
+
+    rm .passwordfile
+}
+
 machine_type=${1}
 server_name_prefix=${2}
 server_count=${3}
-ssh_priv_key_b64=${4}
+payara_admin_password=${4}
+ssh_priv_key_b64=${5}
 
 install_packages    # Should be done on both controller and server
 
 if [ "$machine_type" = "controller" ]; then
     download_payara
     install_ssh_private_key $ssh_priv_key_b64
-    start_payara_admin_server
+    setup_payara_admin_server $payara_admin_password
+    setup_payara_servers $payara_admin_password $server_name_prefix $server_count
 fi
